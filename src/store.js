fff9e071df58aae707e1a96f1a787699374ac09f
DaanKorver <daank2000@hotmail.nl>
2022-03-20 23:48:08 +0100

    update store.js

--- src/store.js
+++ src/store.js
@@ -1,6 +1,7 @@
 import { reactive } from "vue"
+import { supabase } from './supabase'
 
-const user = localStorage.getItem('supabase.auth.token')
+const user = supabase.auth.user()
 
 export const store = reactive({
   user: user || {},
